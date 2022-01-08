a61308c195c5a28577beaf139ad79c95b6faa61e
Taurinz359 <workshere359@gmail.com>
2022-01-08 10:46:07 +0300

    test

--- public/index.php
+++ public/index.php
@@ -9,8 +9,7 @@ $app = AppFactory::create();
 $app ->get('/', function (Request $request, Response $response, $args){
     $response->getBody()->write("Hello world!");
     return $response;
-//
-//
+
 });
 
 $app->run();
