afae94b5dc1be0498bbc2e2a303bb853a1cc4738
Taurinz359 <workshere359@gmail.com>
2022-02-01 10:58:17 +0300

    тестирую middleware

--- app/Middleware/AuthMiddleware.php
+++ app/Middleware/AuthMiddleware.php
@@ -2,15 +2,20 @@
 
 namespace App\Middleware;
 
+use http\Cookie;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\MiddlewareInterface;
 use Psr\Http\Server\RequestHandlerInterface;
+use Slim\Psr7\Cookies;
 
 class AuthMiddleware implements MiddlewareInterface
 {
+
+
     public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
     {
+        $request->withCookieParams(['lowara' => 'ny lox che']);
         return $handler->handle($request);
     }
 }
